996f35ef6043881aed44922fe1c36001e4cad502
An-Hos <Sosnowiec2018>
2018-10-26 16:30:48 +0200

    Update problem006.js

--- problem006.js
+++ problem006.js
@@ -1,5 +1,5 @@
 function divisible (a){
-    for(var i = 1; i < arguments.lenght; i++){
+    for(var i = 1; i < arguments.length; i++){
       if(a % arguments[i] !== 0) return false;
     }
     return true;
