bf06211340e6ae8c2d722e636c2ce3c9b34bf442
An-Hos <Sosnowiec2018>
2018-10-26 15:35:42 +0200

    Update problem001.js

--- problem001.js
+++ problem001.js
@@ -2,8 +2,8 @@ function triangleArea(a,b,c){
     var p;
     var P;
     if(a<=0 || b<=0 || c<=0) return (-1);
-    else if ((a+b) < c && (a+c) < b && (b+c) <a ) return (-1);
-    else{
+    else if (c > (a+b)  || b > (a+c) || a > (b+c)) return (-1);
+    else {
     p = (a+b+c)/2;
     P = Math.sqrt(p*(p-a)*(p-b)*(p-c));
     
