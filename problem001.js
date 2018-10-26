c4702106a898f298d3e3ef4c2b6da13b6d31a4df
An-Hos <Sosnowiec2018>
2018-10-26 15:53:24 +0200

    update problem003

--- problem001.js
+++ problem001.js
@@ -4,9 +4,8 @@ function triangleArea(a,b,c){
     if(a<=0 || b<=0 || c<=0) return (-1);
     else if (c > (a+b)  || b > (a+c) || a > (b+c)) return (-1);
     else {
-    p = (a+b+c)/2;
-    P = Math.sqrt(p*(p-a)*(p-b)*(p-c));
-    
-    return (Math.round(P*100)/100);
+        p = (a+b+c)/2;
+        P = Math.sqrt(p*(p-a)*(p-b)*(p-c));
+        return (Math.round(P*100)/100);
     }  
   }
--- problem002.js
+++ problem002.js
@@ -1,7 +1,7 @@
 function stringRotate(a,b){
     var c;
     if (b >= 0){
-    c = a.substring(0,b);
-    return (a.substring(b)+c); 
+        c = a.substring(0,b);
+        return (a.substring(b)+c); 
     } else return (""); 
   }
