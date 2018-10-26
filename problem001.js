1bcf8a98026ec9ff19dd04ad87463d1818933383
An-Hos <Sosnowiec2018>
2018-10-26 15:14:32 +0200

    Update problem001.js

---
+++ problem001.js
@@ -0,0 +1,11 @@
+function triangleArea(a,b,c){
+    var p;
+    var P;
+    if(a<=0 || b<=0 || c<=0) return (-1);
+    else {
+    p = (a+b+c)/2;
+    P = Math.sqrt(p*(p-a)*(p-b)*(p-c));
+    
+    return (Math.round(P*100)/100);
+    }  
+  }
