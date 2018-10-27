bbb2d55a490b58aeb1c8654ba7891e37e323ca94
An-Hos <Sosnowiec2018>
2018-10-27 12:37:56 +0200

    Update problem007.js

---
+++ problem007.js
@@ -0,0 +1,7 @@
+function findMax(){
+    var max = arguments[0];
+        for(var i = 0; i < arguments.length; i++){
+            if(max < arguments[i]) max = arguments[i];
+        }
+      return (max);
+}
